9f44c51dab511bbdc6d59ea183fc61b7bf89b77c
Anton Chan <chanankuan1@gmail.com>
2023-12-04 18:44:54 +0200

    Add lazy suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,28 @@
-import Home from 'pages/Home';
-import Cast from 'components/Movies/Cast/Cast';
-import MovieDetails from 'components/Movies/MovieDetails/MovieDetails';
-import Movies from 'pages/Movies';
-import Reviews from 'components/Movies/Reviews/Reviews';
+import { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './Layout/Layout';
+const Home = lazy(() => import('pages/Home'));
+const Cast = lazy(() => import('components/Movies/Cast/Cast'));
+const MovieDetails = lazy(() =>
+  import('components/Movies/MovieDetails/MovieDetails')
+);
+const Movies = lazy(() => import('pages/Movies'));
+const Reviews = lazy(() => import('components/Movies/Reviews/Reviews'));
+const Layout = lazy(() => import('components/Layout/Layout'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Suspense>
   );
 };
