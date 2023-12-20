edc91b715990f988f5234954ab823e7546fd9339
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-12-20 02:01:15 +0200

    Update Favorites.jsx

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const favorites = () => {
-  return <h1>This is Favorites page</h1>;
+  return <h1>Here will be Favorites page</h1>;
 };
-
-favorites.propTypes = {};
 
 export default favorites;
