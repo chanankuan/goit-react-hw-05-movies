361400397c3263d82a73e6df7b3ad86920aa93db
Anton Chan <chanankuan1@gmail.com>
2023-12-21 02:24:55 +0200

    Favorite page logic

--- src/api-service/favorites-service.js
+++ src/api-service/favorites-service.js
@@ -1,1 +1,18 @@
-export const addMovie = () => {};
+import axios from 'axios';
+// import { transformMovieData } from 'helpers';
+import { options } from 'constants';
+
+// get movie details by id
+export const getMovieDetails = async movieIds => {
+  const arrayOfPromises = movieIds.map(async movieId => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+      options
+    );
+
+    return data;
+  });
+
+  const movies = await Promise.all(arrayOfPromises);
+  return movies;
+};
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import addIcon from '../assets/images/circle-plus-solid.svg';
+import { Container, Section } from 'components/Common';
+import { getMovieDetails } from 'api-service/favorites-service';
+import MovieList from 'components/MovieList/MovieList';
 
-const favorites = () => {
-  return <h1>Here will be Favorites page</h1>;
+export const Favorites = () => {
+  const [movies, setMovies] = useState([]);
+  const [movieIds, setmovieIds] = useState(
+    JSON.parse(localStorage.getItem('movieIds')) || []
+  );
+
+  useEffect(() => {
+    getMovieDetails(movieIds).then(data => setMovies(data));
+  }, [movieIds]);
+
+  return (
+    <Section>
+      {movieIds.length > 0 ? (
+        <Container>
+          <MovieList movies={movies} />
+        </Container>
+      ) : (
+        <div
+          style={{
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '100%',
+          }}
+        >
+          <Container style={{ textAlign: 'center' }}>
+            <h1 style={{ marginBottom: 10 }}>
+              Your watchlist is looking a little empty...
+            </h1>
+            <Link
+              to="/movies"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                columnGap: 10,
+              }}
+            >
+              <img src={addIcon} alt="Add icon" /> Let's add some movies!
+            </Link>
+          </Container>
+        </div>
+      )}
+    </Section>
+  );
 };
 
-export default favorites;
+export default Favorites;
