553e23d9fad0c3a1119a87aba9ecc2b8a4abc660
chanankuan <106675165+chanankuan@users.noreply.github.com>
2023-12-21 00:31:34 +0000

    Deploying to gh-pages from @ chanankuan/goit-react-hw-05-movies@5820e8af2e32e1b1b8db0c60fc42f487050f23a1 🚀

--- static/js/395.2caeba77.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[395],{7395:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"Here will be Favorites page"})}}}]);
-//# sourceMappingURL=395.2caeba77.chunk.js.map
